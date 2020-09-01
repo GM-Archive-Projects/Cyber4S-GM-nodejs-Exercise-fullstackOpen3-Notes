29ab37fcf69a78451831f947ba540b5e7713e38c
MrMoshkovitz <gal@gmail.com>
2020-09-01 16:48:00 +0300

    uibuild

---
+++ build/precache-manifest.55ba872c7722e6e09eff7bba3cbc5a96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2174b315580b85b57343c31b5f03df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2df330ddcb0f8b16e36b",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2df330ddcb0f8b16e36b",
+    "url": "/static/js/main.ce5a6214.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const cors = require('cors')
+app.use(express.static('build'))
+
 
 const morgan = require('morgan')
 
